e362f2ef28ddb0619d7dd70dd2518e415386abbe
SuperPox <calebkohl@gmail.com>
2021-02-16 14:50:01 -0700

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -43,11 +43,11 @@ def can_be_created_in_a_block(args)
 end
 
 def can_get_the_first_item_in_the_database
-  movies.first
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
@@ -55,7 +55,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  first = Movie.first
 end
 
 def can_find_by_multiple_attributes
@@ -63,13 +63,13 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  #Movie.find_by (title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  __
+  Movie.where(release_date: 2000)
 end
 
 def can_be_found_updated_and_saved
